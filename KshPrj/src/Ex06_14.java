587dac5508dd557a50c823c2717779536993feb8
Kimsuhyen <suhyen21@naver.com>
2021-04-13 09:44:19 +0900

    Ex06_14

---
+++ KshPrj/src/Ex06_14.java
@@ -0,0 +1,13 @@
+public class Ex06_14 {
+	public static void main(String[] args) {
+
+		for (int i = 2; i < 10; i++) {
+			System.out.printf("[" + i + "]´Ü");
+
+			for (int j = 1; j < 10; j++) {
+				System.out.printf(i + " x " + j + " = " + (i * j));
+			}
+		}
+
+	}
+}
