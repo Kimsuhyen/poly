40bd97f69de4bcd8e3b4c1d96143e6bdaed9c545
Kimsuhyen <suhyen21@naver.com>
2021-05-12 09:28:57 +0900

    Codeup_1014

---
+++ KshPrj/src/Codeup_1014.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Codeup_1014 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next();
+		String b = sc.next();
+		System.out.println(b + " " + a);
+	}
+}
