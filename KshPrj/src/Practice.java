687a0d24ccbd356c5b05f145f27a0a4d2347a642
Kimsuhyen <suhyen21@naver.com>
2021-05-21 10:14:13 +0900

    Practice

---
+++ KshPrj/src/Practice.java
@@ -0,0 +1,12 @@
+public class Practice {
+	public static void main(String[] args) {
+		int hap = 0;
+		int i;
+
+		for (i = 3; i < 9; i++) {
+			hap += i;
+		}
+
+		System.out.printf("3에서 8까지의 합: %d \n", hap);
+	}
+}
