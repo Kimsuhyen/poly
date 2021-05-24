5c12aae8066373806cac7cc64a8a5bf1e0b90430
Kimsuhyen <suhyen21@naver.com>
2021-05-25 08:53:13 +0900

    Practice2

---
+++ KshPrj/src/Practice2.java
@@ -0,0 +1,12 @@
+public class Practice2 {
+	public static void main(String[] args) {
+		int i;
+
+		for (i = 1; i < 11; i++) {
+			if (i % 3 == 0) {
+				System.out.printf("[3ÀÇ ¹è¼ö] %d \n", i);
+			} else
+				System.out.println(i);
+		}
+	}
+}
