d02fee6f122e5ad0804174c48745ae67a223d4ff
Kimsuhyen <suhyen21@naver.com>
2021-05-24 09:05:43 +0900

    Codeup_1015

---
+++ KshPrj/src/Codeup_1015.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Codeup_1015 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		float f = sc.nextFloat();
+		System.out.println(String.format("%.2f", f));
+	}
+}
