18da826261c370270a2eb4f1795411a8f51e90b7
Kimsuhyen <suhyen21@naver.com>
2021-05-28 09:27:39 +0900

    Practice4

---
+++ KshPrj/src/Practice4.java
@@ -0,0 +1,19 @@
+public class Practice4 {
+
+	public static void main(String[] args) {
+		int i = 3, j;
+		while (i < 9) {
+			if (i == 3 || i == 5)
+				System.out.println("<" + i + "´Ü>");
+			j = 1;
+			while (j < 10) {
+				System.out.println(i + " x " + j + " = " + i * j);
+				j++;
+			}
+			i++;
+			System.out.println("");
+
+		}
+
+	}
+}
