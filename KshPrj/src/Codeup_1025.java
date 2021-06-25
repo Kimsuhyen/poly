5d7a428baa4c6d33be8bf02be03141395f366422
Kimsuhyen <suhyen21@naver.com>
2021-06-25 16:06:39 +0900

    Codeup_1025

---
+++ KshPrj/src/Codeup_1025.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Codeup_1025 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next();
+		char[] arr = a.toCharArray();
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print("[" + arr[i]);
+			for (int j = arr.length - 1; j > i; j--) {
+				System.out.print("0");
+			}
+			System.out.println("]");
+		}
+
+	}
+
+}
