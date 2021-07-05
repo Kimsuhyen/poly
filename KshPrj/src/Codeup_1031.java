2fc25d7447087bae4336a9a7ac08d68cd904bcb8
Kimsuhyen <suhyen21@naver.com>
2021-07-05 16:00:49 +0900

    Codeup_1031

---
+++ KshPrj/src/Codeup_1031.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Codeup_1031 {
+	public static void main(String[] args) {
+
+        Scanner sc = null;
+				int num = sc.nextInt();
+        System.out.printf("%o", num);
+    }
+}
