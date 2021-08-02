02d589acefa95a5c54563eed6ea41bb40163cf15
Kimsuhyen <suhyen21@naver.com>
2021-08-02 17:29:09 +0900

    Codeup_1036

---
+++ KshPrj/src/Codeup_1036.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Codeup_1036 {
+	public static void main(String[] args) {
+		
+        Scanner sc = new Scanner(System.in);
+        char alpa = sc.nextLine().charAt(0);
+        int ialpa = (int)alpa;
+        System.out.println(ialpa); 
+    } 
+}
